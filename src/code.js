adb1b1139c90eb6283dd35aa13812c3c3fd20515
sevinatenine <141868176+sevinatenine@users.noreply.github.com>
2026-06-24 15:21:22 -0600

    Changed

---
+++ src/code.js
@@ -0,0 +1,125 @@
+async function run(file, inFunc, outFunc, codeError) {
+    var lines = file.split("\n").map(a => a.trim());
+
+    var variables = [0, 0, 0, 0];
+    var stack = [];
+
+    const varMap = [
+        "$",
+        "%",
+        "^",
+        "@"
+    ];
+
+    function shift(val) {
+        variables.splice(0, 0, val);
+        variables.pop();
+    }
+
+    function convVar(name) {
+        return variables[varMap.indexOf(name)];
+    }
+
+    function roll(d) {
+        return (d + 256) % 256;
+    } 
+
+    const parse = (data) => /[$%^@]/.test(data) ? convVar(data) : Number(data);
+
+    const labels = {};
+
+    for (var pc = 0; pc < lines.length; pc++) {
+        if (lines[pc][0] == "!") {
+            if (lines[pc].split("!").filter(e => e).length === 0) {
+                if (labels[lines[pc].trim()]) {
+                    codeError("Label already exists", pc);
+                } else {
+                    //console.log(lines[pc].trim(), pc);
+                    labels[lines[pc].trim()] = pc;   
+                }
+            } else {
+                codeError("Labels can only be !'s.", pc);
+            }
+        }
+    }
+    
+    for (var pc = 0; pc < lines.length; pc++) {
+        var line = lines[pc];
+
+        // console.log(pc, line);
+
+        if (line[0] == "=") {
+            var expr = line.slice(1);
+            var args = expr.split(/[+\-><&]/);
+            
+            if(args.length > 2 || !args.length) {
+                codeError("Expected = <var|const> [<operator> <var|const>]. I.E. = 8 + %, or ^", pc);
+            }
+            
+            if (args.length == 1) { // Single ->     = 8
+                shift(parse(args[0].trim()));
+            } else { // Operator ->     = $ + 7
+                var op = expr.slice(args[0].length, args[0].length+1);
+                
+                args = args.map(e => e.trim());
+                
+                args = args.map(parse);
+                
+                var evaluated = 
+                op == "+" ? args[0] + args[1] :
+                op == "-" ? args[0] - args[1] :
+                op == "&" ? 1 - Math.sign(args[0] * args[1]) :
+                op == "<" ? Number(args[0] < args[1]) :
+                op == ">" ? Number(args[0] > args[1]) : 
+                (() => {codeError("Unknown op: " + op); })();
+                
+                shift(evaluated);
+            }
+        } else if (line[0] == ".") {
+            var data = line.slice(1);
+            data = data.trim();
+            
+            if (data.length < 1) {
+                codeError("Expected. <var|const>. I.E. . $", pc);
+            }
+
+            var evaluated = parse(data);
+            
+            outFunc(String.fromCharCode(evaluated));
+        } else if (line[0] == ",") {
+            shift(inFunc());
+        } else if (line[0] == "~") {
+            let variable = line.slice(1).trim();
+
+            // console.log("Var", variable);
+
+            if(variable.length == 0) {
+                codeError("A variable is required for pushing.", pc)
+            } else stack.push(parse(variable));
+            // console.log("Pushed", stack);
+        } else if (line[0] == "`") {
+            let e = stack.pop();
+            shift(e ?? 0);
+            // console.log("Popped", e, stack);
+        } else if (line[0] == ":") {
+            if (variables[0] === 0) pc++;            
+        } else if (line[0] == "|") {
+            shift(Math.floor(Math.random() * 256));
+        } else if (line[0] == "#" || line[0] == "!") continue;
+        else if (line[0] == "?") {
+            let lbl = line.slice(1).trim();
+
+            if(!(lbl in labels)) codeError("No such label", pc);
+
+            //console.log(labels[lbl]);
+
+            pc = labels[lbl];
+        }
+
+        variables = variables.map(roll);
+        stack = stack.map(roll);
+
+    }
+}
+
+exports.run = run;
---
+++ src/main.js
@@ -0,0 +1,153 @@
+const fs = require("fs");
+const code = require("./code");
+const { exit } = require("process");
+
+const possibleOptions = {
+    "-o": {
+        usage: "-o <stdout_file>",
+        desc: "outputs to a file instead of stdout"
+    },
+    "-i": {
+        usage: "-i <stdin_file>",
+        desc: "takes stdin from a file"
+    },
+    "-h": {
+        usage: "-h",
+        desc: "shows help messages"
+    }
+};
+
+const args = process.argv.slice(2);
+let data = {};
+
+function logOutput(...data) {
+
+}
+
+function logError(...data) {
+    console.error(...data);
+}
+
+function fatal(...data) {
+    logError(`[FATAL] `, ...data);
+    exit(1);
+}
+
+function fatalNoExit(...data) {
+    logError(`[FATAL] `, ...data);
+}
+ 
+function showHelp() {
+    logError("Usage:");
+    logError(`  bachwash.js <filename> [options]`);
+    logError("Options:");
+
+    for (var i of Object.keys(possibleOptions)) {
+        logError(`  ${i}\t ${possibleOptions[i].desc}\n    Usage: ${possibleOptions[i].usage}`);
+    }
+
+    exit(1);
+}
+
+function codeError(err, lineNumber) {
+    logError(`Error on line ${lineNumber + 1}: ${err}`);
+    exit(1);
+}
+
+for (i = 0; i < args.length; i++) {
+    if (args[i] == "-h") {
+        showHelp();
+    }
+    
+    if(args[i] == "-o") {
+        if(data.out) {
+            fatal("Output file already specified.");
+        }
+        
+        if(args[i + 1]) {
+            data.out = args[i + 1];
+            i++;
+            continue;
+        }
+
+        fatal("No output file specified.");
+    } 
+    
+    if(args[i] == "-i") {
+        if(data.in) {
+            fatal("Stdin input file already specified.");
+        }
+        
+        if(args[i + 1]) {
+            data.in = args[i + 1];
+            i++;
+            continue;
+        }
+        
+        fatal("No stdin input file specified.");
+    }
+
+    if (!data.prog) {
+        data.prog = args[i];
+        
+        continue;
+    }
+    
+    fatal("Program file already specified.")
+}
+
+if (!data.prog) {
+    fatalNoExit("No program file specified");
+    showHelp();
+}
+
+try {
+    var file = fs.readFileSync(data.prog, "utf-8");
+} catch (e) {
+    fatal("Failed to open program file.");
+}
+
+let inFd = 0;   // stdin
+let outFd = 1;  // stdout
+
+if (data.out) {
+    try {
+        outFd = fs.openSync(data.out, "w");
+    } catch (e) {
+        fatal("Failed to open out file.");
+    }
+}
+
+if (data.in) {
+    try {
+        inFd = fs.openSync(data.in, "r");
+    } catch (e) {
+        fatal("Failed to open input file.");
+    }
+}
+
+function inFunc() {
+    const buf = Buffer.alloc(1);
+    const bytes = fs.readSync(inFd, buf, 0, 1, null);
+
+    if (bytes === 0) return 0;
+
+    const val = buf[0];
+
+    // strip newline + carriage return
+    if (val === 10 || val === 13) {
+        return inFunc(); // skip it and read next byte
+    }
+
+    return val;
+}
+
+function outFunc(char) {
+    try {
+        fs.writeSync(outFd, char);
+    } catch (e) {
+        console.error("How the **ck did you **ck it up that **cking bad?????? (I mean heck)", e);
+    }
+}
+
+code.run(file, inFunc, outFunc, codeError);
